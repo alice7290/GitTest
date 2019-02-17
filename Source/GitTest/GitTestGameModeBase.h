a7c9231fc1106f05e38211c6b38ab595e4a7fd0c
alice7290 <729043247@qq.com>
2019-02-17 14:41:25 +0800

    ...

--- Source/GitTest/GitTestGameModeBase.h
+++ Source/GitTest/GitTestGameModeBase.h
@@ -1,4 +1,4 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+﻿// Fill out your copyright notice in the Description page of Project Settings.
 
 #pragma once
 
@@ -13,8 +13,8 @@ UCLASS()
 class GITTEST_API AGitTestGameModeBase : public AGameModeBase
 {
 	GENERATED_BODY()
-	
-	
+
+	int32 ABC;
 	
 	
 };
--- Source/GitTest/Public/GitTestActor.h
+++ Source/GitTest/Public/GitTestActor.h
@@ -1,4 +1,4 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+﻿// Fill out your copyright notice in the Description page of Project Settings.
 
 #pragma once
 
@@ -23,6 +23,8 @@ public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
-	
+protected:
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "GitTest")
+	bool bGitTest = true;
 	
 };
